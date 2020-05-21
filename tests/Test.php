c42d86172a705d82886b4d04b30b1e587920c64f
twinh <twinhuang@qq.com>
2020-05-21 16:41:51 +0800

    chore: 测试 redis

--- tests/Test.php
+++ tests/Test.php
@@ -1,9 +1,25 @@
 <?php
 
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 class Test extends \PHPUnit\Framework\TestCase
 {
     public function testCount()
     {
         $this->assertSame(2, 1 + 1);
+    }
+
+    public function testRedis()
+    {
+        $wei = wei([
+           'redis' => [
+               'port' => getenv('REDIS_PORT'),
+           ]
+        ]);
+
+        $redis = $wei->redis;
+
+        $redis->set('a', 'b');
+        $this->assertEquals('b', $redis->get('a'));
     }
 }
