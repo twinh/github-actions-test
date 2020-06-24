d21dc07055433a3a2a5277cf00a5623db122ec2a
twinh <twinhuang@qq.com>
2020-06-25 00:40:25 +0800

    chore: 测试本地发布

--- release.config.js
+++ release.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/github',
-    '@semantic-release/git',
+    '@semantic-release/release-notes-generator'
   ]
 }
