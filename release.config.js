dcf02b1adcc54bc212449724ba19c4090d7031b8
twinh <twinhuang@qq.com>
2020-07-02 15:52:11 +0800

    build: 使用完整的发布命令

--- release.config.js
+++ release.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator'
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@monorepo-semantic-release/git',
   ]
 }
