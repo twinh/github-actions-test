f64ee011a14ad89508cbef003c71e41e2366a21c
twinh <twinhuang@qq.com>
2020-07-14 14:39:18 +0800

    chore: 加回 npm

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@monorepo-semantic-release/monorepo',
+    '@semantic-release/npm',
     '@monorepo-semantic-release/github',
     '@monorepo-semantic-release/git',
   ],
