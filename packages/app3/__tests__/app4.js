be53349bc0fb2e8f1404c6c341bc401681e52805
twinh <twinhuang@qq.com>
2021-03-23 13:25:47 +0800

    build: 增加 app2， app3 build, 依赖

---
+++ packages/app3/__tests__/app4.js
@@ -0,0 +1,11 @@
+const math = require('@github-test/base');
+
+describe('math', () => {
+    test('add', function () {
+        expect(math.add(2, 3)).toBe(5);
+    });
+
+    test('sub', function () {
+        expect(math.add(2, 3)).toBe(5);
+    });
+});
