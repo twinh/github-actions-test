ac8efc5766cae8c4bd19e7bcce9d70216cf37f9f
twinh <twinhuang@qq.com>
2021-04-26 16:19:38 +0800

    build: 支持 patch

--- release.config.js
+++ release.config.js
@@ -3,14 +3,33 @@ module.exports = {
     [
       '@semantic-release/commit-analyzer',
       {
-        "preset": "conventionalcommits"
+        "preset": "conventionalcommits",
+        "releaseRules": [
+          {"type": "patch", "release": "patch"},
+        ],
       },
     ],
     [
       '@semantic-release/release-notes-generator',
       {
-        "preset": "conventionalcommits"
-      }
+        "preset": "conventionalcommits",
+        "presetConfig": {
+          "types": [
+            {type: 'feat', section: 'Features'},
+            {type: 'fix', section: 'Bug Fixes'},
+            {type: 'patch', section: 'Patches'},
+            {type: 'perf', section: 'Performance Improvements'},
+            {type: 'revert', section: 'Reverts'},
+            {type: 'docs', section: 'Documentation', hidden: true},
+            {type: 'style', section: 'Styles', hidden: true},
+            {type: 'chore', section: 'Miscellaneous Chores', hidden: true},
+            {type: 'refactor', section: 'Code Refactoring', hidden: true},
+            {type: 'test', section: 'Tests', hidden: true},
+            {type: 'build', section: 'Build System', hidden: true},
+            {type: 'ci', section: 'Continuous Integration', hidden: true},
+          ],
+        },
+      },
     ],
     '@semantic-release/changelog',
     '@monorepo-semantic-release/monorepo',
